1f0ae6821fe60764423696fab58932809d8f278a
SukunDev <sukundev32@gmail.com>
2022-11-03 18:38:55 +0700

    tambah email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Carbon\Carbon;
 use App\Models\Alat;
 use App\Models\UserNotification;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,80 +27,163 @@ class Kernel extends ConsoleKernel
                         $jadwalAlat = $alat->jadwal
                             ->where('status', 'jadwal baru')
                             ->first();
-                        if ($jadwalAlat->status == 'jadwal baru') {
-                            $jadwalKalibrasi = Carbon::parse(
-                                $jadwalAlat->jadwal_kalibrasi
-                            );
-                            $numberDays = $jadwalKalibrasi->diffInDays(
-                                Carbon::now()->subDay()
-                            );
-                            if ($numberDays == 30) {
-                                $alat->status_kalibrasi = 'persiapan kalibrasi';
-                                $alat->save();
-                                UserNotification::create([
-                                    'user_id' => $alat->user_id,
-                                    'message' =>
-                                        '<a href="/dashboard/alat/' .
-                                        $alat->id .
-                                        '"><p><b>' .
-                                        $alat->nama_alat .
-                                        ' - ' .
-                                        $alat->merk .
-                                        '</b> membutuhkan kalibrasi dalam ' .
-                                        $numberDays .
-                                        ' hari</p></a>',
-                                ]);
-                            } elseif ($numberDays == 15) {
-                                $alat->status_kalibrasi = 'persiapan kalibrasi';
-                                $alat->save();
-                                UserNotification::create([
-                                    'user_id' => $alat->user_id,
-                                    'message' =>
-                                        '<a href="/dashboard/alat/' .
-                                        $alat->id .
-                                        '"><p><b>' .
-                                        $alat->nama_alat .
-                                        ' - ' .
-                                        $alat->merk .
-                                        '</b> membutuhkan kalibrasi dalam ' .
-                                        $numberDays .
-                                        ' hari</p></a>',
-                                ]);
-                            } elseif ($numberDays <= 7) {
-                                if ($numberDays < 1) {
-                                    UserNotification::create([
-                                        'user_id' => $alat->user_id,
-                                        'message' =>
-                                            '<a href="/dashboard/alat/' .
-                                            $alat->id .
-                                            '"><p>Status <b>' .
-                                            $alat->nama_alat .
-                                            ' - ' .
-                                            $alat->merk .
-                                            '</b> Menjadi Kadaluarsa</p></a>',
-                                    ]);
-                                    $jadwalAlat->status = 'kadaluarsa';
-                                    $jadwalAlat->save();
-                                    $alat->status_kalibrasi = 'kadaluarsa';
-                                    $alat->save();
-                                } else {
-                                    $alat->status_kalibrasi =
-                                        'persiapan kalibrasi';
-                                    $alat->save();
-                                    UserNotification::create([
-                                        'user_id' => $alat->user_id,
-                                        'message' =>
-                                            '<a href="/dashboard/alat/' .
-                                            $alat->id .
-                                            '"><p><b>' .
-                                            $alat->nama_alat .
-                                            ' - ' .
-                                            $alat->merk .
-                                            '</b> membutuhkan kalibrasi dalam ' .
-                                            $numberDays .
-                                            ' hari</p></a>',
-                                    ]);
+                        $jadwalKalibrasi = Carbon::parse(
+                            $jadwalAlat->jadwal_kalibrasi
+                        );
+                        $numberDays = $jadwalKalibrasi->diffInDays(
+                            Carbon::now()->subDay()
+                        );
+                        if ($numberDays == 30) {
+                            $alat->status_kalibrasi = 'persiapan kalibrasi';
+                            $alat->save();
+                            if ($alat->user->email) {
+                                Mail::send(
+                                    'emails.pemberitahuanEmails',
+                                    [
+                                        'nama_alat' => $alat->nama_alat,
+                                        'id' => $alat->id,
+                                        'days' => $numberDays,
+                                    ],
+                                    function ($message) use (
+                                        $alat,
+                                        $numberDays
+                                    ) {
+                                        $message->to($alat->user->email);
+                                        $message->subject(
+                                            'Alat ' .
+                                                $alat->nama_alat .
+                                                ' Membutuhkan Kalibrasi Dalam ' .
+                                                $numberDays .
+                                                ' Hari'
+                                        );
+                                    }
+                                );
+                            }
+                            UserNotification::create([
+                                'user_id' => $alat->user_id,
+                                'message' =>
+                                    '<a href="/dashboard/alat/' .
+                                    $alat->id .
+                                    '"><p><b>' .
+                                    $alat->nama_alat .
+                                    ' - ' .
+                                    $alat->merk .
+                                    '</b> membutuhkan kalibrasi dalam ' .
+                                    $numberDays .
+                                    ' hari</p></a>',
+                            ]);
+                        } elseif ($numberDays == 15) {
+                            $alat->status_kalibrasi = 'persiapan kalibrasi';
+                            $alat->save();
+                            if ($alat->user->email) {
+                                Mail::send(
+                                    'emails.pemberitahuanEmails',
+                                    [
+                                        'nama_alat' => $alat->nama_alat,
+                                        'id' => $alat->id,
+                                        'days' => $numberDays,
+                                    ],
+                                    function ($message) use (
+                                        $alat,
+                                        $numberDays
+                                    ) {
+                                        $message->to($alat->user->email);
+                                        $message->subject(
+                                            'Alat ' .
+                                                $alat->nama_alat .
+                                                ' Membutuhkan Kalibrasi Dalam ' .
+                                                $numberDays .
+                                                ' Hari'
+                                        );
+                                    }
+                                );
+                            }
+                            UserNotification::create([
+                                'user_id' => $alat->user_id,
+                                'message' =>
+                                    '<a href="/dashboard/alat/' .
+                                    $alat->id .
+                                    '"><p><b>' .
+                                    $alat->nama_alat .
+                                    ' - ' .
+                                    $alat->merk .
+                                    '</b> membutuhkan kalibrasi dalam ' .
+                                    $numberDays .
+                                    ' hari</p></a>',
+                            ]);
+                        } elseif ($numberDays <= 7) {
+                            if ($numberDays < 1) {
+                                if ($alat->user->email) {
+                                    Mail::send(
+                                        'emails.pemeberitahuanKadaluarsaEmail',
+                                        [
+                                            'nama_alat' => $alat->nama_alat,
+                                            'id' => $alat->id,
+                                        ],
+                                        function ($message) use ($alat) {
+                                            $message->to($alat->user->email);
+                                            $message->subject(
+                                                'Status Alat ' .
+                                                    $alat->nama_alat .
+                                                    ' Telah Menjadi Kadaluarsa'
+                                            );
+                                        }
+                                    );
                                 }
+                                UserNotification::create([
+                                    'user_id' => $alat->user_id,
+                                    'message' =>
+                                        '<a href="/dashboard/alat/' .
+                                        $alat->id .
+                                        '"><p>Status <b>' .
+                                        $alat->nama_alat .
+                                        ' - ' .
+                                        $alat->merk .
+                                        '</b> Menjadi Kadaluarsa</p></a>',
+                                ]);
+                                $jadwalAlat->status = 'kadaluarsa';
+                                $jadwalAlat->save();
+                                $alat->status_kalibrasi = 'kadaluarsa';
+                                $alat->save();
+                            } else {
+                                $alat->status_kalibrasi = 'persiapan kalibrasi';
+                                $alat->save();
+                                if ($alat->user->email) {
+                                    Mail::send(
+                                        'emails.pemberitahuanEmails',
+                                        [
+                                            'nama_alat' => $alat->nama_alat,
+                                            'id' => $alat->id,
+                                            'days' => $numberDays,
+                                        ],
+                                        function ($message) use (
+                                            $alat,
+                                            $numberDays
+                                        ) {
+                                            $message->to($alat->user->email);
+                                            $message->subject(
+                                                'Alat ' .
+                                                    $alat->nama_alat .
+                                                    ' Membutuhkan Kalibrasi Dalam ' .
+                                                    $numberDays .
+                                                    ' Hari'
+                                            );
+                                        }
+                                    );
+                                }
+                                UserNotification::create([
+                                    'user_id' => $alat->user_id,
+                                    'message' =>
+                                        '<a href="/dashboard/alat/' .
+                                        $alat->id .
+                                        '"><p><b>' .
+                                        $alat->nama_alat .
+                                        ' - ' .
+                                        $alat->merk .
+                                        '</b> membutuhkan kalibrasi dalam ' .
+                                        $numberDays .
+                                        ' hari</p></a>',
+                                ]);
                             }
                         }
                     }
